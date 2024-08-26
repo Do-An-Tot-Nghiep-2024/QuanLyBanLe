fe65f04bfcb7f4689948964aa751ddd88382741a
vietbac <bacnguyendev2002@gmail.com>
2024-08-26 22:00:19 +0700

    add supplier entity for backend

---
+++ backend/src/main/java/bac/dev/se/backend/repositories/SupplierRepository.java
@@ -0,0 +1,9 @@
+package bac.dev.se.backend.repositories;
+
+import bac.dev.se.backend.models.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+}
