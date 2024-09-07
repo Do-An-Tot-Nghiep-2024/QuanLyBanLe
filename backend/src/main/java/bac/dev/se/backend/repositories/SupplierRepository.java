1b7f53a1ebc67f3fa4ddae8e063deee219fd51e7
vietbac <bacnguyendev2002@gmail.com>
2024-09-07 10:38:44 +0700

    remove backend folder

--- backend/src/main/java/bac/dev/se/backend/repositories/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package bac.dev.se.backend.repositories;
-
-import bac.dev.se.backend.models.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-}
