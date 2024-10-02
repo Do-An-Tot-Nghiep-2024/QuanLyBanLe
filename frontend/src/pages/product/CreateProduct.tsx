baac47c95ace59cd384bf72443038862270374be
VuLanTuong <lantuonglearning@gmail.com>
2024-10-02 23:01:45 +0700

    update form product

--- frontend/src/pages/product/CreateProduct.tsx
+++ frontend/src/pages/product/CreateProduct.tsx
@@ -119,6 +119,7 @@ export default function CreateProduct() {
           mb={2}
           sx={{ justifyContent: "center" }}
         >
+<<<<<<< Updated upstream
           <Button
             type="button"
             sx={{
@@ -129,6 +130,98 @@ export default function CreateProduct() {
               padding: "6px 12px", // Decrease padding
             }}
             onClick={handleBack} // Call handleBack to navigate back
+=======
+          Thêm sản phẩm mới
+        </Typography>
+
+        <Container
+          component={"form"}
+          onSubmit={handleSubmit}
+          sx={{
+            boxShadow: 3,
+            borderRadius: 2,
+            padding: 5,
+            backgroundColor: "white",
+            width: "80%",
+            margin: "auto",
+          }}
+        >
+          {error && <Alert severity="error">{error}</Alert>}
+
+          <Stack spacing={2} mb={2} sx={{ alignItems: "center" }}>
+            <FormControl sx={{ width: "60%" }}>
+              <FormLabel htmlFor="name" sx={{ textAlign: "left" }}>
+                Tên nhà cung cấp:
+              </FormLabel>
+              <Select
+                name="name"
+                variant="outlined"
+                displayEmpty
+                native // Sử dụng native để tạo dropdown menu
+              >
+                <option value="" disabled>
+                  Chọn nhà cung cấp
+                </option>
+                <option value="supplier1">Nhà cung cấp 1</option>
+                <option value="supplier2">Nhà cung cấp 2</option>
+                <option value="supplier3">Nhà cung cấp 3</option>
+                {/* Thêm các option khác tương ứng với các nhà cung cấp khác */}
+              </Select>
+            </FormControl>
+
+            <FormControl sx={{ width: "60%" }}>
+              <FormLabel htmlFor="name" sx={{ textAlign: "left" }}>
+                Danh mục sản phẩm:
+              </FormLabel>
+              <Select
+                name="name"
+                variant="outlined"
+                displayEmpty
+                native // Sử dụng native để tạo dropdown menu
+              >
+                <option value="" disabled>
+                  Chọn danh mục sản phẩm
+                </option>
+                <option value="supplier1">Danh mục 1</option>
+                <option value="supplier2">Danh mục 2</option>
+                <option value="supplier3">Danh mục 3</option>
+                {/* Thêm các option khác tương ứng với các nhà cung cấp khác */}
+              </Select>
+            </FormControl>
+
+            <FormControl sx={{ width: "60%" }}>
+              <FormLabel htmlFor="name" sx={{ textAlign: "left" }}>
+                Tên sản phẩm:
+              </FormLabel>
+              <TextField name="name" variant="outlined" />
+            </FormControl>
+            
+            <FormControl sx={{ width: "60%" }}>
+            <FormLabel htmlFor="name" sx={{ textAlign: "left" }}>
+              Giá sản phẩm:
+            </FormLabel>
+            <TextField name="name" variant="outlined" />
+          </FormControl>
+
+            <FormControl sx={{ width: "60%" }}>
+              <FormLabel htmlFor="productImage" sx={{ textAlign: "left" }}>
+                Hình ảnh sản phẩm:
+              </FormLabel>
+              <Input
+                accept="image/*"
+                id="productImage"
+                name="productImage"
+                type="file"
+              />
+            </FormControl>
+          </Stack>
+
+          <Stack
+            direction="row"
+            spacing={2}
+            mb={2}
+            sx={{ justifyContent: "center" }}
+>>>>>>> Stashed changes
           >
             Quay lại
           </Button>
