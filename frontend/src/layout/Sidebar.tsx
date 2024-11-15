fe8f2c27f1a67c6dca96a8c437d5b3c477499030
VuLanTuong <lantuonglearning@gmail.com>
2024-11-15 23:56:24 +0700

    handle update ui

--- frontend/src/layout/Sidebar.tsx
+++ frontend/src/layout/Sidebar.tsx
@@ -105,11 +105,28 @@ const NAVIGATION: Navigation = [
     segment: "employees",
     title: "Nhân viên",
     icon: <BadgeOutlinedIcon />,
-  },  
+  },
   {
     segment: "customers",
     title: "Khách hàng",
     icon: <PeopleAltOutlinedIcon />,
+  },
+  {
+    segment: "promotions",
+    title: "Chương trình khuyến mãi",
+    icon: <NoteAddOutlined />,
+    children: [
+      {
+        segment: "",
+        title: "Quản lí chương trình khuyến mãi",
+        icon: <WarehouseIcon />,
+      },
+      {
+        segment: "create-promotion",
+        title: "Tạo chương trình khuyến mãi",
+        icon: <AddBusinessIcon />,
+      },
+    ],
   },
   {
     segment: "reports",
